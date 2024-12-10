19cec7ef4b3ea05752023cb13e4df80659e842da
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2024-12-10 10:49:04 +0530

    add error handling route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import userRouter from "./routes/user.route"
+import { notFoundErrorHandler } from "./controllers/error.controller";
+import { errorHandler } from "./middleware/error.middleware";
+
+
 const app=express();
 
 
 app.use(express.json());
 
 app.use("/api/v1/user",userRouter);
+
+app.all("*",notFoundErrorHandler)
+app.use(errorHandler);
 
 export default app;
